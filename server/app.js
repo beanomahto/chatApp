af004ad2c992899d006d1e72932752e802e70505
beanomahto <beanomahto9031@gmail.com>
2025-06-08 22:50:55 +0530

    frontend deployed

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const server = require("http").createServer(app);
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://charcha-app-client.onrender.com",
     methods: ["GET", "POST"],
   },
 });
